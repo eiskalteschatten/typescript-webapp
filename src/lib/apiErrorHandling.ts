eccd34fd999c111d7a6587abf3e8e14a410890c5
Alex <alex@alexseifert.com>
2019-06-15 22:41:08 +0200

    Update apiErrorHandling lib

--- src/lib/apiErrorHandling.ts
+++ src/lib/apiErrorHandling.ts
@@ -5,11 +5,6 @@ import HttpError from '../errors/HttpError';
 
 
 export function returnError(error: HttpError, req: Request, res: Response): void {
-    if (error.status) {
-        res.status(error.status).send(error.message);
-    }
-    else {
-        console.error(error);
-        res.status(500).send(translate(req.lang, 'anErrorOccurred'));
-    }
+    console.error(error);
+    res.status(error.status || 500).send(error.message || translate(req.lang, 'anErrorOccurred'));
 }

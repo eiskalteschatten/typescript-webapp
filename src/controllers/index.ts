65be1991836d1f86ca680b1be50a4e325dbc0e22
Alex <alex@alexseifert.com>
2019-11-30 20:49:17 +0100

    Fix spacing

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,4 @@
-import  { Router, Request, Response } from 'express';
+import { Router, Request, Response } from 'express';
 
 import { translate } from '../lib/translate';
 import track from '../lib/matomo';
